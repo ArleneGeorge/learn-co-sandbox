9cd6952eae7365848129b44715178863fc734ab0
Arlene George <aemg@live.com>
2019-08-27 23:15:33 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,37 @@
+
+
+# stuff = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
+# results = stuff.select {|item| item.start_with?"a"}
+# print results
+
+
+def num_of_words(string)
+  string.split.length
+end
+puts num_of_words("Hi, isn't this a great and interesting sentence??")
+
+
+# def rude_greeting(name=nil)
+# name ||= "you jerk"
+# puts "Hey there, #{name}"
+# end
+
+# print rude_greeting
+
+# def my_favorite_animal
+#   "cat"
+# end
+ 
+# best_animal = my_favorite_animal
+ 
+# puts best_animal
+
+# puts "Blink" + 182.to_s
+
+
+foods = {"pie" => "delicious", "broccoli" => "not delicious",
+"carrots" => "not delicious", "apples" => "delicious",
+"peanut butter" => "delicious"}
+
+
+
---
+++ learning-comments.rb
@@ -0,0 +1,46 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+ # puts "code inside"
+#end
+#puts "code after if...end"
+
+#chance_of_rain = 0.2
+#if chance_of_rain <= 0.25
+ # puts "Pack a sunshelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75 )
+ # puts "Pack an umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+#end
+
+#current_weather = "raining"
+
+#case current_weather
+#when "sunny"
+ # puts "grab some sunscreen!!"
+#when "raining"
+ # puts "grab an umbrella"
+#when "snowing"
+ # puts "bundle up"
+#end
+
+#magic_exit_number = 7
+#count = 0
+
+# while count < 10 do 
+  # break if count == magic_exit_number
+  # puts "I am the #{count}, I love to count!"
+  # count += 1
+ # end 
+
+#3.times do
+ # puts "I ran."
+#end
+
+
+counter = 0 
+until counter == 20 
+  puts "the current number is less than 20."
+  counter += 1
+end
